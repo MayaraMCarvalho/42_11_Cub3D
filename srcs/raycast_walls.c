6dec571615d88d8757b3ae0dcd97a68099aa22cf
macarval <macarval@student.42sp.org.br>
2024-02-29 13:56:04 -0300

    [CUB3D]: simple colors

--- srcs/raycast_walls.c
+++ srcs/raycast_walls.c
@@ -6,7 +6,7 @@
 /*   By: macarval <macarval@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 17:50:49 by joapedr2          #+#    #+#             */
-/*   Updated: 2024/02/29 11:06:21 by macarval         ###   ########.fr       */
+/*   Updated: 2024/02/29 13:53:25 by macarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,15 @@ void	get_walls(t_data *game, t_raycast *ray)
 	float	max;
 	float	tam;
 	float	distance;
+	int		color;
 
 	distance = ray->ver[2];
+	color = 0x111e99;
 	if (distance > ray->hor[2])
+	{
+		color = 0x7202b8;
 		distance = ray->hor[2];
+	}
 	max = (float) WIN_H;
 	if (max < WIN_W)
 		max = WIN_W;
@@ -28,5 +33,5 @@ void	get_walls(t_data *game, t_raycast *ray)
 	tam = ((1.2 * max) - 20) * (max - 0.1) / (distance - 0.1) / 100;
 	if (tam - floor(tam) > 0.4)
 		tam += 1.0;
-	draw_walls(&game->img, tam, ray->rays, 0x111e99);
+	draw_walls(&game->img, tam, ray->rays, color);
 }
